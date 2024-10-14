2fada7ca2dbdea20ded82321e36b294c1bb6cd1e
Ayush Prajapati <162590519+Silent-Ame@users.noreply.github.com>
2024-10-14 15:22:25 +0530

    Add files via upload

---
+++ For_Loop.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b;
+    string numbers[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    cin >> a;
+    cin >> b;
+
+    for (int i = a; i <= b; i++) {
+        if (i <= 9) {
+            cout << numbers[i] << std::endl;
+        } else {
+            if (i % 2 == 0) {
+                cout << "even" << std::endl;
+            } else {
+                cout << "odd" << std::endl;
+            }
+        }
+    }
+    
+    return 0;
+}
